555b49ffa52a238e690ab1c35a153397e493bf1c
MagicPrince666 <846863428@qq.com>
2024-09-17 22:01:59 +0800

    add rslidar

---
+++ launch/rs16_mapping.launch.py
@@ -0,0 +1,89 @@
+import os.path
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    package_path = get_package_share_directory('fast_lio')
+    default_config_path = os.path.join(package_path, 'config')
+    default_rviz_config_path = os.path.join(
+        package_path, 'rviz', 'fastlio.rviz')
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    config_path = LaunchConfiguration('config_path')
+    config_file = LaunchConfiguration('config_file')
+    rviz_use = LaunchConfiguration('rviz')
+    rviz_cfg = LaunchConfiguration('rviz_cfg')
+    imu_cfg = os.path.join(get_package_share_directory('ros2_imu'), 'params', 'imu_zyf176ex_cfg.yaml')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time', default_value='false',
+        description='Use simulation (Gazebo) clock if true'
+    )
+    declare_config_path_cmd = DeclareLaunchArgument(
+        'config_path', default_value=default_config_path,
+        description='Yaml config file path'
+    )
+    decalre_config_file_cmd = DeclareLaunchArgument(
+        'config_file', default_value='rs16.yaml',
+        description='Config file'
+    )
+    declare_rviz_cmd = DeclareLaunchArgument(
+        'rviz', default_value='true',
+        description='Use RViz to monitor results'
+    )
+    declare_rviz_config_path_cmd = DeclareLaunchArgument(
+        'rviz_cfg', default_value=default_rviz_config_path,
+        description='RViz config file path'
+    )
+
+    imu_node = Node(
+        namespace='/',
+        package='ros2_imu',
+        executable='ros2_imu_node',
+        name='ros2_imu_node',
+        parameters=[imu_cfg],
+        output='screen'
+    )
+
+    rslidar_node = Node(
+        namespace='rslidar_sdk',
+        package='rslidar_sdk',
+        executable='rslidar_sdk_node',
+        output='screen'
+    )
+
+    fast_lio_node = Node(
+        package='fast_lio',
+        executable='fastlio_mapping',
+        parameters=[PathJoinSubstitution([config_path, config_file]),
+                    {'use_sim_time': use_sim_time}],
+        output='screen'
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_cfg],
+        condition=IfCondition(rviz_use)
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_config_path_cmd)
+    ld.add_action(decalre_config_file_cmd)
+    ld.add_action(declare_rviz_cmd)
+    ld.add_action(declare_rviz_config_path_cmd)
+
+    ld.add_action(imu_node)
+    ld.add_action(rslidar_node)
+    ld.add_action(fast_lio_node)
+    ld.add_action(rviz_node)
+
+    return ld
